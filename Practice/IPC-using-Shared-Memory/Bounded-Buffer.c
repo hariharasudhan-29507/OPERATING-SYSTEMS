fcb4c8af716ba2aad81092f922e7f71d57b9acb5
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-08-03 11:27:12 +0530

    Create Bounded-Buffer.c

---
+++ Practice/IPC-using-Shared-Memory/Bounded-Buffer.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
+
+#define SIZE 10
+
+struct shared {
+    int buffer[SIZE];
+    int in;
+    int out;
+    int count;
+};
+
+int main() {
+    int shmid,i;
+    struct shared *shm;
+
+    
+    shmid = shmget(IPC_PRIVATE, sizeof(struct shared), IPC_CREAT | 0666);
+
+    if (shmid < 0) {
+        printf("Shared memory creation failed\n");
+        exit(1);
+    }
+
+    shm = (struct shared *)shmat(shmid, NULL, 0);
+
+    shm->in = 0;
+    shm->out = 0;
+    shm->count = 0;
+
+    pid_t pid = fork();
+
+    if (pid == 0) {
+       
+        int i,input[SIZE] ;
+	for(i = 0 ;i<SIZE ;i++ ){
+		printf("Enter data(CHILD) : ");
+		scanf("%d",&input[i]);
+	}
+
+        printf("Producer (Child):\n");
+
+        for (i = 0; i < SIZE; i++) {
+
+            if (shm->count == SIZE) {
+                printf("Buffer Full\n");
+                break;
+            }
+
+            shm->buffer[shm->in] = input[i];
+
+            printf("Produced %d at index %d\n",
+                   input[i], shm->in);
+
+            shm->in = (shm->in + 1) % SIZE;
+            shm->count++;
+        }
+
+        shmdt(shm);
+        exit(0);
+    }
+    else {   
+        wait(NULL);
+	wait(NULL);
+        printf("\nConsumer (Parent):\n");
+
+        while (shm->count > 0) {
+
+            printf("Consumed %d from index %d\n",
+                   shm->buffer[shm->out],
+                   shm->out);
+
+            shm->out = (shm->out + 1) % SIZE;
+            shm->count--;
+        }
+
+        shmdt(shm);
+        shmctl(shmid, IPC_RMID, NULL);
+    }
+
+    return 0;
+}
