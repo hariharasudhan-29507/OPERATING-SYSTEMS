f3f762823ded37366a934d5099c5e6c6652bfa5b
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-09-07 11:41:52 +0530

    Create 1114.PrintInOrder.c

---
+++ Leetcode/1114.PrintInOrder.c
@@ -0,0 +1,40 @@
+#include <semaphore.h>
+#include <stdlib.h>
+
+typedef struct {
+    sem_t first_done;
+    sem_t second_done;
+} Foo;
+
+void printFirst();
+void printSecond();
+void printThird();
+
+Foo* fooCreate() {
+    Foo* obj = (Foo*) malloc(sizeof(Foo));
+    sem_init(&obj->first_done, 0, 0);
+    sem_init(&obj->second_done, 0, 0);
+    return obj;
+}
+
+void first(Foo* obj) {
+    printFirst();
+    sem_post(&obj->first_done);
+}
+
+void second(Foo* obj) {
+    sem_wait(&obj->first_done);
+    printSecond();
+    sem_post(&obj->second_done);
+}
+
+void third(Foo* obj) {
+    sem_wait(&obj->second_done);
+    printThird();
+}
+
+void fooFree(Foo* obj) {
+    sem_destroy(&obj->first_done);
+    sem_destroy(&obj->second_done);
+    free(obj);
+}
